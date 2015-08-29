700dc0f8a7a2a643c5e5e3a98b5b34760ee09426
ios5436 <ios5436@Mac-mini-de-Caelum-10.local>
2015-08-29 10:03:24 -0300

    Initial Commit

---
+++ ContatosIP67/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContatosIP67
+//
+//  Created by ios5436 on 29/08/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ContatosIP67/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios5436 on 29/08/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
