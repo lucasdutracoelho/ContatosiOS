6bdf52a5b76303c7b5d358d1bad3efbfcb264ba8
ios5436 <ios5436@Mac-mini-de-Caelum-10.local>
2015-09-12 16:34:55 -0300

    Criacao de novas classes

---
+++ ContatosIP67/GerenciadorDeAcoes.m
@@ -0,0 +1,13 @@
+//
+//  GerenciadorDeAcoes.m
+//  ContatosIP67
+//
+//  Created by ios5436 on 12/09/15.
+//  Copyright (c) 2015 Caelum. All rights reserved.
+//
+
+#import "GerenciadorDeAcoes.h"
+
+@implementation GerenciadorDeAcoes
+
+@end
